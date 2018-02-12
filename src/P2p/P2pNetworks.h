ef1996d6b0ae1fa165d983df66624941c6099b01
selva <support@selvahost.us>
2018-02-12 13:27:31 -0300

    .

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid MARKETCASH_NETWORK = { { 0x92, 0x01, 0x62, 0x51, 0x80, 0x83, 0x09, 0x41, 0x89, 0x13, 0x76, 0x14, 0x02, 0x59, 0x56, 0x52 } };
+}
